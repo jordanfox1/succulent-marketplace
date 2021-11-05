ad06b8bd10916ba911afb27e97aa277cad5c0185
jordanfox1 <jordanfox9415@gmail.com>
2021-11-06 00:19:32 +1000

    troubleshooting deployment

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -63,6 +63,7 @@ class ListingsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
       @listing = Listing.find(params[:id])
+      @listings = Listing.all
     end
 
     # Only allow a list of trusted parameters through.
